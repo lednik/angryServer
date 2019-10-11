9f5e1a968211a8abeb83db2e054d5b22039bd9d2
Nikolay <lednick008@gmail.com>
2019-10-11 18:49:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var word = document.getElementById('word');
+var button = document.getElementById('button');
+button.addEventListener('click',function(){
+    let pet= word.value;
+    console.log(pet);
+})
